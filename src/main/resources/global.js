9a164d879c58dad7a97d6a27f0df47195c50cc89
821600682@qq.com <821600682@qq.com>
2015-08-04 13:50:48 +0800

    改PushToWeb的目标地址

--- src/main/resources/global.js
+++ src/main/resources/global.js
@@ -1,5 +1,5 @@
 {
 	/*where log files place, absolute path*/
 	daili_db_file_path: "/data/sqlite/daili/daili.db",
-	dailiweb_post_url: "http://192.168.1.105:3000/",
+	dailiweb_post_url: "http://52.69.206.42:3000/",
 }
